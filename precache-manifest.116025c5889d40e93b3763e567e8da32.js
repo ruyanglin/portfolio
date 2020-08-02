f70d742e9a09d06155cf217dfdc555978b51945a
Ryan Lin <ruyanglin@gmail.com>
2020-08-02 18:29:49 -0400

    Updates

---
+++ precache-manifest.116025c5889d40e93b3763e567e8da32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3029c66b8219de0cec72b13dfd119c4f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bff1f5e49e997b48af6c",
+    "url": "/portfolio/static/css/main.b5ae18ad.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff1f5e49e997b48af6c",
+    "url": "/portfolio/static/js/main.990f7e9b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
