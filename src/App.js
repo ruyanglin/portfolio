b70f40bec746f9e09d2fe1352f3e67f9819c7cd5
Ryan Lin <ruyanglin@gmail.com>
2020-08-02 18:19:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Ryan Lin. Welcome to my portfolio! I'm currently still under construction.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
