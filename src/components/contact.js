df37fcc722154ae7760c55474bd0033a6ad53ee1
Ryan Lin <ruyanglin@gmail.com>
2020-08-06 18:07:02 -0400

    replace contact info with images

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import file from '../images/file_icon.png';
+import linkedin from '../images/linkedin.png';
+import github from '../images/github.png';
+import instagram from '../images/insta.png';
+
 
 class Contact extends React.Component {
     render() {
         return (
         <div className="App-contact">
-          <a href="https://drive.google.com/file/d/1SGVATAfiGqU2U2RXWRr4-Q-FUJ6g_LoO/view?usp=sharing"> Resume </a> 
-          <a href="https://www.linkedin.com/in/ruyanglin/"> LinkedIn </a>
-          <a href="https://www.github.com/ruyanglin"> GitHub </a>
-          <a href="https://www.instagram.com/timecrop/"> Instagram </a>
+          <a href="https://drive.google.com/file/d/1SGVATAfiGqU2U2RXWRr4-Q-FUJ6g_LoO/view?usp=sharing"> <img className="contact-img" src={file} alt="file icon"/> </a>
+          <a href="https://www.linkedin.com/in/ruyanglin/"> <img className="contact-img" src={linkedin} alt="linkedin icon"/>  </a>
+          <a href="https://www.github.com/ruyanglin"> <img className="contact-img" src={github} alt="github icon"/>  </a>
+          <a href="https://www.instagram.com/timecrop/"> <img className="contact-img" src={instagram} alt="instagram icon"/>  </a>
         </div>
         )
     }
