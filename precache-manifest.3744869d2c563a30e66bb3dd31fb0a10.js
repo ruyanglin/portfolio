11b766273b75dd7eb40bc62d8b4e38ef94ed6c5d
Ryan Lin <ruyanglin@gmail.com>
2020-08-07 02:24:52 -0400

    Updates

---
+++ precache-manifest.3744869d2c563a30e66bb3dd31fb0a10.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96853810f32a0968e7bb70f70f97fe1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d62794478361d8d4ea8e",
+    "url": "/portfolio/static/css/main.c0159057.chunk.css"
+  },
+  {
+    "revision": "9ffb2d64484f4e591b88",
+    "url": "/portfolio/static/js/2.bde85f68.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.bde85f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62794478361d8d4ea8e",
+    "url": "/portfolio/static/js/main.3f161c0b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b6cbd3a713d6eecf38d56dbfa9a6f46c",
+    "url": "/portfolio/static/media/citco.b6cbd3a7.jpg"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/portfolio/static/media/insta.0e84d592.png"
+  },
+  {
+    "revision": "cf4b32ec282a81046575937dc7326744",
+    "url": "/portfolio/static/media/linkedin2.cf4b32ec.png"
+  },
+  {
+    "revision": "d7623bdc7431f44979e343fbd162493c",
+    "url": "/portfolio/static/media/ryan.d7623bdc.png"
+  },
+  {
+    "revision": "2d4a0035d4e4b7fe24fe48f9bddf4274",
+    "url": "/portfolio/static/media/uw.2d4a0035.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.024ec458528dfe084f40f8b49f86f637.js"
+  "/portfolio/precache-manifest.3744869d2c563a30e66bb3dd31fb0a10.js"
 );
 
 self.addEventListener('message', (event) => {
